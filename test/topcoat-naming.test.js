1f82714a1315368233bca7cba61d57b1fe753364
garthdb <garthdb@gmail.com>
2017-08-24 16:35:46 -0600

    added support for custom naming functions

--- test/topcoat-naming.test.js
+++ test/topcoat-naming.test.js
@@ -4,6 +4,28 @@ import TopcoatNaming from '../src/index';
 import fs from 'fs';
 import {_outdent} from '../src/topcoat-naming.js';
 
+function customSelectorNaming ({block, element, modifier, state}) {
+  const resultAr = [block];
+  if(element) resultAr.push(element);
+  if(modifier) resultAr.push(modifier);
+  let result = '.';
+  result += resultAr.map((part) => {
+    return part.replace(/\b\w/g, l => l.toUpperCase());
+  }).join('');
+  if(state) {
+    const stateRegex = /^:?(\w+)$/;
+    const matches = state.match(stateRegex);
+    const cleanStateName = matches[1];
+    // has colon for dom states (i.e. :disabled)
+    if((matches[1] != matches[0])) {
+      result += `${state}, ${result}__${cleanStateName}`;
+    } else {
+      result += `__${cleanStateName}`;
+    }
+  }
+  return result;
+}
+
 function runTopcoatNaming(input, opts) {
   return postcss([
     TopcoatNaming(opts),
@@ -258,6 +280,83 @@ test('Modifier Topdoc collapsing', (t) => {
   const input = fs.readFileSync('./test/fixtures/topdoc-simple.css', 'utf8');
   const expected = fs.readFileSync('./test/expected/topdoc-modifier.css', 'utf8');
   return runTopcoatNaming(input, {modifier: 'secondary'})
+    .then((result) => {
+      t.deepEqual(result.css, expected);
+    });
+});
+
+test('Custom Selector Name Function', (t) => {
+  const input =
+  `@block Button {
+  color: #333;
+  @modifier secondary {
+    color: #666;
+    @state :hover {
+      outline: none;
+    }
+  }
+  @state :disabled {
+    opacity: 0.1;
+  }
+}`;
+  const expected =
+  `.ButtonSecondary {
+  color: #333;
+  color: #666;
+}
+.ButtonSecondary:disabled, .ButtonSecondary__disabled {
+  opacity: 0.1;
+}
+.ButtonSecondary:hover, .ButtonSecondary__hover {
+  outline: none;
+}`;
+  const opts = {
+    modifier: 'secondary',
+    selectorNaming: customSelectorNaming
+  }
+  return runTopcoatNaming(input, opts)
+    .then((result) => {
+      t.deepEqual(result.css.trim(), expected);
+    });
+});
+
+test('Custom Markup Class Name Function', (t) => {
+  const input = fs.readFileSync('./test/fixtures/topdoc-simple.css', 'utf8');
+  const expected = `/* topdoc
+name: Secondary Button
+description: a secondary button
+markup: |
+  <button class="ButtonSecondary"/>
+  <button class="ButtonSecondary__disabled"/>
+*/
+.ButtonSecondary {
+  color: #333;
+  color: #666;
+}
+.ButtonSecondary:disabled, .ButtonSecondary__disabled {
+  opacity: 0.1;
+}
+`;
+  const opts = {
+    modifier: 'secondary',
+    selectorNaming: customSelectorNaming,
+    domNaming: function({block, element, modifier, state}) {
+      const resultAr = [block];
+      if(element) resultAr.push(element);
+      if(modifier) resultAr.push(modifier);
+      let result = resultAr.map((part) => {
+        return part.replace(/\b\w/g, l => l.toUpperCase());
+      }).join('');
+      if(state) {
+        const stateRegex = /^:?(\w+)$/;
+        const matches = state.match(stateRegex);
+        const cleanStateName = matches[1];
+        result += `__${cleanStateName}`;
+      }
+      return result;
+    }
+  };
+  return runTopcoatNaming(input, opts)
     .then((result) => {
       t.deepEqual(result.css, expected);
     });
